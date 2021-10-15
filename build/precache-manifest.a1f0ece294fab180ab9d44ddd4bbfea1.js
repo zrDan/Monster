89558f658a9ce9d3a3761d4b69ec698e9dc609a9
Daniel Garcia <business_dan@protonmail.com>
2021-10-15 15:15:50 -0500

    build

---
+++ build/precache-manifest.a1f0ece294fab180ab9d44ddd4bbfea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27b5d7c1eed5676d991f669f991cb82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96fda96c23089eef9beb",
+    "url": "/static/css/main.b9a67b7b.chunk.css"
+  },
+  {
+    "revision": "9c3aa2a64560af29feaa",
+    "url": "/static/js/2.cc99bfa2.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/static/js/2.cc99bfa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fda96c23089eef9beb",
+    "url": "/static/js/main.66375e3d.chunk.js"
+  },
+  {
+    "revision": "c334259c93d3af278983",
+    "url": "/static/js/runtime-main.efc94dc8.js"
+  }
+]);
